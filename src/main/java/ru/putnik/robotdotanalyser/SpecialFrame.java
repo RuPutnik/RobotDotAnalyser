9f225baa118e66a0ec0e80c37cda69702710b0aa
Putnik13 <ilja.rumjancev@lenta.ru>
2018-07-22 13:05:52 +0300

    Create project. commit 1

---
+++ src/main/java/ru/putnik/robotdotanalyser/SpecialFrame.java
@@ -0,0 +1,35 @@
+package ru.putnik.robotdotanalyser;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class SpecialFrame extends Application implements Initializable {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent=FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("view/SpecialFrame.fxml")));
+        primaryStage.setScene(new Scene(parent));
+
+        primaryStage.setResizable(false);
+        primaryStage.setWidth(250);
+        primaryStage.setHeight(100);
+        primaryStage.setTitle("Координаты курсора");
+        primaryStage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
