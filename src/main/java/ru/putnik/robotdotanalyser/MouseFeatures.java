43c50e4fab71f7b8cff062b2a6716d6c831a0a8f
Putnik13 <ilja.rumjancev@lenta.ru>
2018-07-22 13:43:02 +0300

    Developping classes and methods. commit 3

---
+++ src/main/java/ru/putnik/robotdotanalyser/MouseFeatures.java
@@ -0,0 +1,30 @@
+package ru.putnik.robotdotanalyser;
+
+import javafx.scene.control.Alert;
+
+import java.awt.*;
+
+public class MouseFeatures {
+    private Robot robot;
+    public MouseFeatures(){
+        try {
+            robot=new Robot();
+        } catch (AWTException e) {
+            Alert errorAlert=new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Инифиализация ядра");
+            errorAlert.setTitle("Ошибка");
+            errorAlert.setContentText("Во время инициализации произошла ошибка!");
+            errorAlert.show();
+            e.printStackTrace();
+        }
+    }
+    public int[] printCoordinates(){
+        int x=0;
+        int y=0;
+
+        return new int[]{x,y};
+    }
+    public void moveCursor(int x,int y){
+
+    }
+}
--- src/main/java/ru/putnik/robotdotanalyser/SpecialFrame.java
+++ src/main/java/ru/putnik/robotdotanalyser/SpecialFrame.java
@@ -1,10 +1,16 @@
 package ru.putnik.robotdotanalyser;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -12,6 +18,13 @@ import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class SpecialFrame extends Application implements Initializable {
+    private MouseFeatures features=new MouseFeatures();
+    @FXML
+    private Button startPrint;
+    @FXML
+    private Button moveCursor;
+    @FXML
+    private TextField coordinatesField;
     public static void main(String[] args) {
         launch();
     }
@@ -28,8 +41,52 @@ public class SpecialFrame extends Application implements Initializable {
         primaryStage.show();
     }
 
+    public int[] getCoordinates(){
+        int[] coordinates=new int[2];
+        String coordsLine=coordinatesField.getText();
+        if(coordsLine.matches("\\d+:\\d+")) {
+            coordinates[0]=Integer.parseInt(coordsLine.split(":")[0]);
+            coordinates[1]=Integer.parseInt(coordsLine.split(":")[1]);
+        }else {
+            Alert errorAlert=new Alert(Alert.AlertType.ERROR);
+            errorAlert.setContentText("Координаты были введены неверно!");
+            errorAlert.setTitle("Ошибка");
+            errorAlert.setHeaderText("Ошибка определения координат");
+            errorAlert.show();
+            coordinatesField.setText("0:0");
+            coordinates[0]=0;
+            coordinates[1]=0;
+        }
+        return coordinates;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        startPrint.setOnAction(new StartPrintCoordinates());
+        moveCursor.setOnAction(new MoveCursorToCoordinates());
     }
+    public class StartPrintCoordinates implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent event) {
+            features.printCoordinates();
+        }
+    }
+    public class MoveCursorToCoordinates implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent event) {
+            if(!coordinatesField.getText().equals("")) {
+                int[] coordinates = getCoordinates();
+                features.moveCursor(coordinates[0], coordinates[1]);
+            }else {
+                Alert errorAlert=new Alert(Alert.AlertType.WARNING);
+                errorAlert.setHeaderText("Поле для координат пусто");
+                errorAlert.setTitle("Ошибка");
+                errorAlert.setContentText("Для переноса курсора нужно указать координаты!");
+                errorAlert.show();
+            }
+        }
+    }
+
 }
